862dd228e188b96a8af31469f749d415fcc35462
Noble <neluwah@gmail.com>
2025-02-15 20:33:03 +0100

    Fix: Add missing HTTPException import

--- api/routes/books.py
+++ api/routes/books.py
@@ -1,7 +1,7 @@
 
 from typing import OrderedDict
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 from fastapi.responses import JSONResponse
 
 from api.db.schemas import Book, Genre, InMemoryDB
